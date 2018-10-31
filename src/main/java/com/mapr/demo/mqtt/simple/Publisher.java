e475650ec2ff153a3df393944aadb48bc6066bdf
masternirms <nirms.rajesh@gmail.com>
2018-10-31 22:19:31 +0000

    Added files of MQTT Publisher to git repository.

--- src/main/java/com/mapr/demo/mqtt/simple/Publisher.java
+++ src/main/java/com/mapr/demo/mqtt/simple/Publisher.java
@@ -3,29 +3,27 @@ package com.mapr.demo.mqtt.simple;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import java.util.Random;
 
 public class Publisher {
 
 
   public static void main(String[] args) throws MqttException {
 
-    String messageString = "Hello World from Java!";
-
-    if (args.length == 2 ) {
-      messageString = args[1];
-    }
-
-
-    System.out.println("== START PUBLISHER ==");
-
-
     MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
     client.connect();
     MqttMessage message = new MqttMessage();
-    message.setPayload(messageString.getBytes());
-    client.publish("iot_data", message);
+    Random rand = new Random();
 
-    System.out.println("\tMessage '"+ messageString +"' to 'iot_data'");
+    for(int i = 0; i <= 10000; i++) {
+
+      int messageInt = rand.nextInt(2);
+      String messageString = Integer.toString(messageInt);
+      message.setPayload(messageString.getBytes());
+      client.publish("iot_data", message);
+
+      System.out.println(messageString);
+    }
 
     client.disconnect();
 
